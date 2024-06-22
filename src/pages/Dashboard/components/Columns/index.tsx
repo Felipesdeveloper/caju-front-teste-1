24bc841f2ab6097ac0193a21003f0fc058702234
FelipeSDeveloper <felipe.sdeveloper@gmail.com>
2024-06-21 21:26:47 -0300

    chore: padroniza formatação da aplicação

--- src/pages/Dashboard/components/Columns/index.tsx
+++ src/pages/Dashboard/components/Columns/index.tsx
@@ -1,11 +1,10 @@
-
-import * as S from "./styles";
-import RegistrationCard from "../RegistrationCard";
+import * as S from './styles';
+import RegistrationCard from '../RegistrationCard';
 
 const allColumns = [
-  { status: 'REVIEW', title: "Pronto para revisar" },
-  { status: 'APPROVED', title: "Aprovado" },
-  { status: 'REPROVED', title: "Reprovado" },
+  { status: 'REVIEW', title: 'Pronto para revisar' },
+  { status: 'APPROVED', title: 'Aprovado' },
+  { status: 'REPROVED', title: 'Reprovado' },
 ];
 
 type Props = {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import eslintPlugin from "@nabla/vite-plugin-eslint";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
