ff5e86e2c266893bf6114569568d8261b080b049
FelipeSDeveloper <felipe.sdeveloper@gmail.com>
2024-06-21 21:42:37 -0300

    chore: remove o .eslintcache

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
+  plugins: [react(), eslintPlugin({ eslintOptions: { cache: false } })],
   server: {
     port: 3001,
   },
