f42a054a359baa0f3a9adee9e71da84398db1a14
FelipeSDeveloper <felipe.sdeveloper@gmail.com>
2024-06-24 22:01:54 -0300

    chore: adiciona formatações

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,16 @@ module.exports = {
     browser: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:import/recommended', 'plugin:jsx-a11y/recommended', 'plugin:@typescript-eslint/recommended', 'eslint-config-prettier', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+  ],
   settings: {
     react: {
       version: 'detect',
