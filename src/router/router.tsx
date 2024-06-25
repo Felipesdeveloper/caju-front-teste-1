1e490d8171e7c0deffc67e2fe637b40f6a2e6b70
FelipeSDeveloper <felipe.sdeveloper@gmail.com>
2024-06-25 13:28:07 -0300

    chore: corrige disponibilização do contexto

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,24 +6,26 @@ import { RegistrationsProvider } from '@/context/registrations/registrationsProv
 
 const Router = () => {
   return (
-    <div style={{ marginTop: 64 }}>
-      <BrowserRouter>
-        <Switch>
-          <RegistrationsProvider>
-            <Route exact path={routes.dashboard} component={DashboardPage} />
-          </RegistrationsProvider>
-          <Route exact path={routes.newUser} component={NewUserPage} />
-          <Route
-            exact
-            path={routes.history}
-            component={() => <div>History</div>}
-          />
-          <Route exact path="*">
-            <Redirect to={routes.dashboard} />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <RegistrationsProvider>
+      <div style={{ marginTop: 64 }}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={routes.dashboard}>
+              <DashboardPage />
+            </Route>
+            <Route exact path={routes.newUser} component={NewUserPage} />
+            <Route
+              exact
+              path={routes.history}
+              component={() => <div>History</div>}
+            />
+            <Route exact path="*">
+              <Redirect to={routes.dashboard} />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </RegistrationsProvider>
   );
 };
 
